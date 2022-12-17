411d55ca3dc8f7979c57ab87cae6120b14f7089f
Ryukkun <tkytwins2@gmail.com>
2022-12-17 22:50:34 +0900

    ⚠️🐎 re

--- pi_yo_6/synthetic_voice.py
+++ pi_yo_6/synthetic_voice.py
@@ -5,12 +5,23 @@ import os
 import wave
 import platform
 
-from .romaji.to_kana import to_kana as Romaji
+from .romaji.to_kana import Romaji
 
 if platform.system() == 'Windows':
     EFormat = 'shift_jis'
 else:
     EFormat = 'utf-8'
+
+re_mention = re.compile(r'<(@&|@)\d+>')
+re_emoji = re.compile(r'<:.+:[0-9]+>')
+re_url = re.compile(r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+')
+re_bot_prefix = re.compile(r'^[,./?!].*')
+re_speed = re.compile(r'speed:\S*\s|speed:\S*\Z')
+re_a = re.compile(r'a:\S*\s|a:\S*\Z')
+re_tone = re.compile(r'tone:\S*\s|tone:\S*\Z')
+re_jf = re.compile(r'jf:\S*\s|jf:\S*\Z')
+re_voice = re.compile(r'voice:\w*\s|voice:\w*\Z')
+
 
 def custam_text(text,path):
 
@@ -33,7 +44,7 @@ def costom_voice(text,config):
     
     htsvoice=['-m',config.OJ.Voice+'mei_normal.htsvoice']
     
-    if r := re.findall(r'voice:\w*\s|voice:\w*\Z', text):
+    if r := re_voice.findall(text):
         r = re.sub(r'\s',"",r[0])
         text = re.sub(r,'',text)
         r = re.sub('voice:','',r)
@@ -43,9 +54,9 @@ def costom_voice(text,config):
     
 #------------------------------------------------------
 
-def costom_status(text,default,prefix,search_r):
+def costom_status(text, default, prefix, _re:re.Pattern):
     
-    if r := re.findall(search_r, text):
+    if r := _re.findall(text):
         r = re.sub(r'\s',"",r[0])
         text = re.sub(r,'',text)
         default[1] = re.sub(prefix,'',r)
@@ -66,7 +77,7 @@ def replace_english_kana(text):
     while word := re.search(r'(^|[^a-zA-Z])([a-zA-Z\-]+)($|[^a-zA-Z])', temp_text):
         #print(f"{temp_text} : {word}")
 
-        if word.start() != 0 or re.compile(r'[^a-zA-Z\-]').search(temp_text[0]):    # 文字のスタート位置修復
+        if word.start() != 0 or re.search(r'[^a-zA-Z\-]',temp_text[0]):    # 文字のスタート位置修復
             output += temp_text[:word.start()+1]
 
         if kana := alkana.get_kana(word.group(2).lower()):      # 英語変換
@@ -78,7 +89,7 @@ def replace_english_kana(text):
         else:
             output += Romaji.to_kana(word.group(2).lower())   # ローマ字 → カナ 変換
         
-        if word.end() != len(temp_text) or re.compile(r'[^a-zA-Z\-]').search(temp_text[-1]):    # 文字の末尾を修復
+        if word.end() != len(temp_text) or re.search(r'[^a-zA-Z\-]',temp_text[-1]):    # 文字の末尾を修復
             temp_text = temp_text[word.end()-1:]
         else:
             temp_text = ""
@@ -89,15 +100,19 @@ def replace_english_kana(text):
 
 
 # ************************************************
+def replace_w(Itext):
+    Itext = re.sub(r'(ww+|ｗｗ+)','わらわら',Itext)
+    Itext = re.sub(r'(w|ｗ)','わら',Itext)
+    return Itext
 
 
-async def creat_voice(Itext,guild_id,now_time,config):
+async def creat_voice(Itext:str, guild_id, now_time, config):
 
     Itext = Itext.replace('\n',' ')
-    Itext = re.sub(r'^[,./?!].*','',Itext)                                                              # コマンドは読み上げない
-    Itext = re.sub(r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+','ユーアールエルは省略するのです！ ',Itext)    # URL省略
-    Itext = re.sub(r'<:.+:[0-9]+>','',Itext)                                                            # 絵文字IDは読み上げない
-    Itext = re.sub(r'<(@&|@)\d+>','メンションは省略するのです！ ',Itext)
+    Itext = re.sub(re_bot_prefix,'',Itext)                                                              # コマンドは読み上げない
+    Itext = re.sub(re_url,'ユーアールエルは省略するのです！ ',Itext)    # URL省略
+    Itext = re.sub(re_emoji,'',Itext)                                                            # 絵文字IDは読み上げない
+    Itext = re.sub(re_mention,'メンションは省略するのです！ ',Itext)
     Itext = custam_text(Itext,config.Admin_dic)                                           # ユーザ登録した文字を読み替える
     Itext = custam_text(Itext,config.User_dic + guild_id + '.txt')
 
@@ -108,7 +123,7 @@ async def creat_voice(Itext,guild_id,now_time,config):
     if ItextTemp == []:
 
         Itext = replace_english_kana(Itext)
-        Itext = re.sub(r'ww+|ｗｗ+','わらわら',Itext)
+        Itext = replace_w(Itext)
         print(f"変換後:{Itext}")
         
         cmd = f'open_jtalk -x "{config.OJ.Dic}" -m "{config.OJ.Voice}mei_normal.htsvoice" -r 1.2 -ow "{config.OJ.Output}{guild_id}-{now_time}.wav"'
@@ -137,14 +152,14 @@ async def creat_voice(Itext,guild_id,now_time,config):
 
 
 async def split_voice(Itext,FileNum,id_time,config):
-    Itext,hts = costom_voice(Itext,config)      #voice
-    Itext,speed = costom_status(Itext,['-r','1.2'],"speed:",r'speed:\S*\s|speed:\S*\Z')     #speed
-    Itext,a = costom_status(Itext,['-a','auto'],"a:",r'a:\S*\s|a:\S*\Z')                    #AllPath
-    Itext,tone = costom_status(Itext,['-fm','auto'],"tone:",r'tone:\S*\s|tone:\S*\Z')       #tone
-    Itext,jf = costom_status(Itext,['-jf','auto'],"jf:",r'jf:\S*\s|jf:\S*\Z')               #jf
+    Itext,hts = costom_voice(Itext,config)                              #voice
+    Itext,speed = costom_status(Itext,['-r','1.2'],"speed:",re_speed)   #speed
+    Itext,a = costom_status(Itext,['-a','auto'],"a:",re_a)              #AllPath
+    Itext,tone = costom_status(Itext,['-fm','auto'],"tone:",re_tone)    #tone
+    Itext,jf = costom_status(Itext,['-jf','auto'],"jf:",re_jf)          #jf
 
     Itext = replace_english_kana(Itext)
-    Itext = re.sub(r'ww+|ｗｗ+','わらわら',Itext)
+    Itext = replace_w(Itext)
     print(f"変換後 ({FileNum+1}) :{Itext}")
 
     FileName = config.OJ.Output+id_time+"-"+str(FileNum)+".wav"
