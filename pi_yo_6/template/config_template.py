f2a92801f82bcc949c0ddb164295f663cfb5434a
Ryukkun <tkytwins2@gmail.com>
2022-12-18 18:48:32 +0900

    ⚠️

---
+++ pi_yo_6/template/config_template.py
@@ -0,0 +1,15 @@
+class Open_Jtalk:
+    Dic = '/var/lib/mecab/dic/open-jtalk/naist-jdic'
+    Voice = './Voice/'
+    Output = './Output/'
+
+class VOICEVOX:
+    
+
+class Config:
+    Prefix = '.'
+    Token = ''
+    Admin_dic = './dic/admin_dic.txt'
+    User_dic = './dic/user_dic/'
+    Guild_Config = './guild_config/'
+    OJ = Open_Jtalk()
