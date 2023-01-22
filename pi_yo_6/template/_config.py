21a9dd51b6c67a8f057edacaa4424ec2c0138a24
Ryukkun <tkytwins2@gmail.com>
2023-01-22 15:22:28 +0900

    ✎ 詳細表記 ☺️

--- pi_yo_6/template/_config.py
+++ pi_yo_6/template/_config.py
@@ -1,19 +1,53 @@
 class Open_Jtalk:
-    # windowsで起動するときだけ Dic_shift_jis 必須
+    '''
+    Dic_utf_8 : str
+    Dic_shift_jis : str
+        windowsで起動するときだけ Dic_shift_jis 必須
+    
+    Voice : str
+        htsvoiceを入れておくファイル
+    
+    Output : str
+        音声ファイル一時的保存場所
+    '''
     Dic_utf_8 = '/var/lib/mecab/dic/open-jtalk/naist-jdic'
     Dic_shift_jis = '/var/lib/mecab/dic/open-jtalk/naist-jdic'
-    # htsvoiceを入れておくファイル
     Voice = './Voice/'
     Output = './Output/'
 
+
 class VOICEVOX:
-    # https://github.com/VOICEVOX/voicevox_core/releases
-    # onnxruntimeを同じフォルダに入れてください https://github.com/microsoft/onnxruntime/releases
+    '''
+    https://github.com/VOICEVOX/voicevox_core/releases
+    onnxruntimeを同じフォルダに入れてください https://github.com/microsoft/onnxruntime/releases
+    
+    -------
+    use_gpu : bool
+        voicevox core の注意点を読んでください https://github.com/VOICEVOX/voicevox_core/tree/main
+
+    load_all_models : bool
+        今のところは True にしておかないと動きません。いつかメモリ使用を抑制するために修正するかも
+    ''' 
     core_windows = './pi_yo_6/voicevox/voicevox_core-windows/core.dll'
     core_linux = './pi_yo_6/voicevox/voicevox_core-linux/libcore.so'
     core_darwin = './pi_yo_6/voicevox/voicevox_core-darwin/libcore.dylib'
-    # use_gpuをオンにする場合は、voicevox core の注意点を読んでください https://github.com/VOICEVOX/voicevox_core/tree/main
+
     use_gpu = False
+    load_all_models = True
+
+
+class Coeiroink:
+    '''
+    https://coeiroink.com/
+    音声モデルは自分で入れてください pi_yo_6/coeiroink/speaker_info
+
+    -------
+    load_all_models : bool
+        False 激推しです。　Trueにしたらメモリ不足で仮想メモリまで侵食します
+        モデル量にもよりますが、自分は 20GB弱くらい取られました
+    ''' 
+    load_all_models = False
+
 
 class Config:
     Prefix = '.'
@@ -24,3 +58,4 @@ class Config:
     User_Config = './user_config/'
     OJ = Open_Jtalk
     VVOX = VOICEVOX
+    Coeiroink = Coeiroink
