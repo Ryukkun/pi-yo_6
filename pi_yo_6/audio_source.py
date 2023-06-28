f71c9ef5753b87e9226ed9ae8385fc1253027b46
Ryukkun <tkytwins2@gmail.com>
2023-06-29 00:06:54 +0900

    🐜 super()

--- pi_yo_6/audio_source.py
+++ pi_yo_6/audio_source.py
@@ -7,6 +7,7 @@ from .voice_client import _StreamAudioData
 class StreamAudioData(_StreamAudioData):
 
     def __init__(self,url):
+        super().__init__()
         self.url = url
         self.loop = asyncio.get_event_loop()
 
